5d515f124740bf12fc5d10cfcf7e0fe57048e405
ZackyDacky <robloxzacfiel@gmail.com>
2021-05-10 14:08:08 +0800

    first commit

---
+++ src/data/repositories/UserRepoImplement.ts
@@ -0,0 +1,33 @@
+import { User } from "../../domain/entities/User";
+import { UserRepository } from "../../domain/repositories/UserRepository";
+import axios from 'axios';
+
+const Axios = axios.create({
+    baseURL: 'https://reqres.in'
+})
+
+class UserDTO {
+    id: number
+    email: string
+    first_name: string
+    last_name: string
+}
+
+export class UserRepoImplement implements UserRepository {
+
+
+    async GetUsers(): Promise<User[]> {
+        console.log('getting users')
+        const response = await Axios.get("/api/users?page=2")
+        console.log('after response')
+        console.log(response.data.data)
+        return response.data.data.map((user: UserDTO) => (
+            {
+                id: user.id,
+                email: user.email,
+                first_name: user.first_name,
+                last_name: user.last_name
+            }
+        ))
+    }
+}
---
+++ src/domain/entities/User.ts
@@ -0,0 +1,13 @@
+export class User {
+    id: number
+    email: string
+    first_name: string
+    last_name: string
+
+    constructor(id: number, email: string, first_name: string, last_name: string) {
+        this.id = id
+        this.email = email
+        this.first_name = first_name
+        this.last_name = last_name
+    }
+}
